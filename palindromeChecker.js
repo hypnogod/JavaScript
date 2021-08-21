72fb328d4d6861115dc6efe50360ca257816a859
hypnogod <62040270+hypnogod@users.noreply.github.com>
2021-08-20 21:26:15 -0400

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -13,4 +13,21 @@ palindrome("My age is 0, 0 si ega ym.");
 
 /*
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+
+***palindrome for numbers only (w/o using strings method)***
+const isReverse = (num) => {
+    let rev = 0;
+    while (num && num > 0) {
+        rev = (rev * 10) + (num%10)
+        num = Math.floor(num/10); 
+    }
+    return rev;
+}
+var palindrome = function(x) {
+    console.log(isReverse(x))
+    return x === isReverse(x);
+};
+
+
 */
